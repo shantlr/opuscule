01c999b26fc6eeedd2d8612a0642ccd0a67e81a2
Patrick Lin <>
2024-12-07 11:15:34 +0100

    fix(bookmarked): scroll view

--- app/app/(tabs)/booked.tsx
+++ app/app/(tabs)/booked.tsx
@@ -1,4 +1,5 @@
 import { ActivityIndicator, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import { BooksGrid } from '@/features/books/ui-books-grid';
 import { useBookmarkedBooks } from '@/features/books/use-book';
@@ -6,13 +7,13 @@ import { useBookmarkedBooks } from '@/features/books/use-book';
 export default function Booked() {
   const { data, isLoading } = useBookmarkedBooks({});
   return (
-    <View>
+    <ScrollView>
       {isLoading && (
         <View className="w-full flex flex-row items-center">
           <ActivityIndicator />
         </View>
       )}
       <BooksGrid books={data?.books} />
-    </View>
+    </ScrollView>
   );
 }
